fb94823105f580fae924ace11d51efb88579ae54
Juan Alcantara <juan@cordage.io>
2022-08-05 23:43:08 -0500

    Lazy Load and React memo

--- src/components/GiftSearch/index.js
+++ src/components/GiftSearch/index.js
@@ -1,10 +1,14 @@
+import debounce from 'just-debounce-it'
 import React, {useCallback, memo} from 'react'
 
 const GiftSearch = ({setSearch}) => {
-  const handleSearch = useCallback((e) => {
-    const keyword = e.target.value !== '' ? e.target.value : null
-    setSearch(keyword)
-  }, [setSearch])
+  const handleSearch = useCallback(debounce(
+    (e) => {
+      const keyword = e.target.value !== '' ? e.target.value : null
+      setSearch(keyword)
+    },
+    500
+  ), [setSearch])
 
   return (
     <input type={'text'} onChange={handleSearch} placeholder={'Search'} style={{marginBlockEnd: '20px'}} />
